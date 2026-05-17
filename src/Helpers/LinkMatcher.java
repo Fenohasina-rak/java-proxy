c7976811b95c13cefd247dfdf1a4e3d8a3e2e540
RAKOTOARITSOA Fenohasina MEA/SAIO/MADAGASCAR/DTI <rakotoaritsoafenohasina@gmail.com>
2026-05-17 18:00:04 +0300

    remove system print and comments

--- src/Helpers/LinkMatcher.java
+++ src/Helpers/LinkMatcher.java
@@ -8,14 +8,11 @@ public class LinkMatcher {
         if (listBlockedDomains.contains(requestTarget)) return true;
         String temp = requestTarget;
         int dotIndex;
-        //Check all subdomains
-        //Maka point voalohany
+
         while ((dotIndex = temp.indexOf('.')) != -1) {
-            //Asorina mot avant anle point voalohany + le point de miloop jusque tsis point tsun
             temp = temp.substring(dotIndex + 1);
             if (listBlockedDomains.contains(temp)) return true;
         }
-        //si pas return true amle loop, false zany
         return false;
     }
 
